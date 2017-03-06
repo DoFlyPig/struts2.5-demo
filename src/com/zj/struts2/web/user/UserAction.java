c38e575191bff5ea53a67df32e5997f1c15711b1
zhangjian <cqzhangjian1988@gmail.com>
2017-03-06 21:26:43 +0800

    第一次提交

---
+++ src/com/zj/struts2/web/user/UserAction.java
@@ -0,0 +1,27 @@
+package com.zj.struts2.web.user;
+
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport {
+
+	public String list() {
+
+		System.out.println("list.....");
+		System.out.println("git");
+		System.out.println("git");
+		System.out.println("git");
+		return "list";
+	}
+	
+	@Action(value = "/add", results = { @Result(
+				name="baidu",location="http://www.baidu.com",type="redirect"
+			)})
+	public String add() {
+		System.out.println("baidu..");
+		return "baidu";
+	}
+
+}
